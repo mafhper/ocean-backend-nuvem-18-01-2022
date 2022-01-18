5ecb3801afa2644e04398e90a3ad6081086e722b
mafhper <mafhper@gmail.com>
2022-01-18 14:26:02 -0300

    MONGO EXPOSED

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ObjectId, HostAddress } = require("mongodb");
-const url = "mongodb://localhost:27017";
+const url =
+  "mongodb+srv://admin:LF5RH2DP58@9CtF@cluster0.3y5kc.mongodb.net/retryWrites=true&w=majority";
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
@@ -11,7 +12,7 @@ async function main() {
   //   const collection = db.collection("herois");
 
   const collection = undefined;
-  
+
   // Aplicação em Express
   const app = express();
 
@@ -19,41 +20,44 @@ async function main() {
   // estará sempre estruturado em JSON
   app.use(express.json());
 
+  // Load css from public folder
+  app.use(express.static("public"));
+
   // Endpoint "/"
-  app.get("/", function (req, res) {
+  app.get("/", (req, res) => {
     res.send("Hello, World!");
   });
 
   // Endpoint "/oi"
-  app.get("/oi", function (req, res) {
-    res.send("Olá, mundo!");
+  app.get("/oi", (req, res) => {
+    res.send("<h1>Oi, mundo!</h1>");
   });
 
   const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
   //              0                   1                2
 
   // [GET] "/herois" - Read All (Ler todos os registros)
-  app.get("/herois", async function (req, res) {
+  app.get("/herois", async (req, res) => {
     const documentos = await collection.find().toArray();
     res.send(documentos);
   });
 
   // [GET] "/herois/:id" - Read Single (by Id) (Ler um registro pelo ID)
-  app.get("/herois/:id", async function (req, res) {
+  app.get("/herois/:id", async (req, res) => {
     const id = req.params.id;
     const item = await collection.findOne({ _id: new ObjectId(id) });
     res.send(item);
   });
 
   // [POST] "/herois" - Create (Criar um registro)
-  app.post("/herois", async function (req, res) {
+  app.post("/herois", async (req, res) => {
     const item = req.body;
     await collection.insertOne(item);
     res.send(item);
   });
 
   // [PUT] "/herois/:id" - Update (Atualizar um registro)
-  app.put("/herois/:id", async function (req, res) {
+  app.put("/herois/:id", async (req, res) => {
     const id = req.params.id;
     const item = req.body;
     await collection.updateOne(
@@ -66,15 +70,16 @@ async function main() {
   });
 
   // [DELETE] "/herois/:id" - Delete (Remover um registro)
-  app.delete("/herois/:id", async function (req, res) {
+  app.delete("/herois/:id", async (req, res) => {
     const id = req.params.id;
     await collection.deleteOne({ _id: new ObjectId(id) });
     res.send("Item removido com sucesso.");
   });
 
   // [LISTEN] - On Heroku and Localhost
-  app.listen(process.env.PORT || 3000, function () {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Server running");
+    console.log(`http://localhost:${process.env.PORT || 3000}`);
   });
 }
 
