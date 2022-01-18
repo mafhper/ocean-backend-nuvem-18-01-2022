f964687fdc68a42d127c5ab08ce800575b15db62
mafhper <mafhper@gmail.com>
2022-01-18 13:44:39 -0300

    Teste de conexão

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const { MongoClient, ObjectId, HostAddress } = require("mongodb");
-
 const url = "mongodb://localhost:27017";
-// const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
-
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
   // Conexão com o Banco de Dados
 
-  const client = await MongoClient.connect(url);
-
-  const db = client.db(dbName);
-
-  const collection = db.collection("herois");
+//   const client = await MongoClient.connect(url);
+//   const db = client.db(dbName);
+//   const collection = db.collection("herois");
 
   // Aplicação em Express
-
   const app = express();
 
   // Sinalizo para o Express que o body das requisições
@@ -39,58 +33,47 @@ async function main() {
   // [GET] "/herois" - Read All (Ler todos os registros)
   app.get("/herois", async function (req, res) {
     const documentos = await collection.find().toArray();
-
     res.send(documentos);
   });
 
   // [GET] "/herois/:id" - Read Single (by Id) (Ler um registro pelo ID)
   app.get("/herois/:id", async function (req, res) {
     const id = req.params.id;
-
     const item = await collection.findOne({ _id: new ObjectId(id) });
-
     res.send(item);
   });
 
   // [POST] "/herois" - Create (Criar um registro)
   app.post("/herois", async function (req, res) {
     const item = req.body;
-
     await collection.insertOne(item);
-
     res.send(item);
   });
 
   // [PUT] "/herois/:id" - Update (Atualizar um registro)
   app.put("/herois/:id", async function (req, res) {
     const id = req.params.id;
-
     const item = req.body;
-
     await collection.updateOne(
       { _id: new ObjectId(id) },
       {
         $set: item,
       }
     );
-
     res.send(item);
   });
 
   // [DELETE] "/herois/:id" - Delete (Remover um registro)
   app.delete("/herois/:id", async function (req, res) {
     const id = req.params.id;
-
     await collection.deleteOne({ _id: new ObjectId(id) });
-
     res.send("Item removido com sucesso.");
   });
 
-  // [LISTEN ON HEROKU and Localhost]
-    app.listen(process.env.PORT || 3000, function () {
-        console.log("Server running");
-        });
-
+  // [LISTEN] - On Heroku and Localhost
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Server running");
+  });
 }
 
 main();
